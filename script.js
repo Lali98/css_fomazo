272d67bda2c391a1ccff564240ff0b627784e1c5
Lali98 <kisslajosbertalan77@gmail.com>
2022-11-03 18:49:39 +0100

    Elso

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById('css_form').onsubmit = (event) => {
+    event.preventDefault();
+    document.getElementsByTagName('style')[0].innerHTML = event.target.elements.cssFormazas.value;
+}
+
+document.getElementById('cssFormazas').addEventListener('keydown', function (event) {
+    if (event.key === 'Tab') {
+        event.preventDefault();
+        let start = this.selectionStart;
+        let end = this.selectionEnd;
+
+        // set textarea value to: text before caret + tab + text after caret
+        this.value = this.value.substring(0, start) +
+            "\t" + this.value.substring(end);
+
+        // put caret at right position again
+        this.selectionStart =
+            this.selectionEnd = start + 1;
+    }
+});
